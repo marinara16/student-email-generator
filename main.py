4cc22acde7840e430b571ebb322a28fe5e6a67d5
marinara16 <maldavaz16@gmail.com>
2025-10-20 18:17:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def generate_email_body(row):
     progress_section = "\n".join(progress_lines)
     upcoming_section = "\n".join(upcoming_lines)
     
-    email_body = f"""Hey {first_name},
+    email_body = f"""
 
 **Current Total: {total_points} points**
 
